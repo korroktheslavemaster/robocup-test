a2688ef17084e3cc4345cadcbbd488f35749a6db
Arpit Saxena <arpit.tarang@gmail.com>
2016-01-30 23:23:31 +0530

    revert changes to formulae

--- fira/comm testing/ssltest.cpp
+++ fira/comm testing/ssltest.cpp
@@ -15,35 +15,30 @@ int* duty_calc(int vel_euc[])
      float R=176;//in mm
      int buf[4];
 
-    // buf[0]=-0.5*vel_euc[0]+0.866*vel_euc[1]+vel_euc[2]*R;
-    // buf[3]=-0.5*vel_euc[0]-0.866*vel_euc[1]+vel_euc[2]*R;
-    // buf[1]=(0.707*vel_euc[0]-0.707*vel_euc[1]+vel_euc[2]*R)/1.3;
-    // buf[2]=(0.707*vel_euc[0]+0.707*vel_euc[1]+vel_euc[2]*R)/1.3;
-     // float factor;
-     // if((127*(0.707*vel_euc[0]-0.707*vel_euc[1]+vel_euc[2]*R)/(2500*3.14/60))<30) factor=1.1;
-     // else if((127*(0.707*vel_euc[0]-0.707*vel_euc[1]+vel_euc[2]*R)/(2500*3.14/60))<60) factor=1.4;
-     // else if((127*(0.707*vel_euc[0]-0.707*vel_euc[1]+vel_euc[2]*R)/(2500*3.14/60))<100) factor=1.6;
-
-     buf[0]=-(-0.5*vel_euc[0]+0.866*vel_euc[1]+vel_euc[2]*R);
+     cout<<"omega="<<vel_euc[2]<<"\n";
+     buf[0]=(-0.5*vel_euc[0]+0.866*vel_euc[1]+vel_euc[2]*R);
      buf[2]=(-0.5*vel_euc[0]-0.866*vel_euc[1]+vel_euc[2]*R);
-     buf[1]=-(0.707*vel_euc[0]-0.707*vel_euc[1]+vel_euc[2]*R);
+     buf[1]=(0.707*vel_euc[0]-0.707*vel_euc[1]+vel_euc[2]*R);
      buf[3]=(0.707*vel_euc[0]+0.707*vel_euc[1]+vel_euc[2]*R);
 
     for (int i = 0; i < 4; ++i)
     {
         cout<<buf[i]<<" ";
     }
-    
     for (int i = 0; i < 4; i++)
     {
         da[i]=(int)(127*buf[i]/(25000*3.14/60));
         printf("%d ",da[i]);
+    }
+
+    /*for (int i = 0; i < 4; i++)
+    {
         if(i==1)
         {
-            if(abs(da[i])<30) da[i]/=1.1;
-            else if(abs(da[i])<=60) da[i]/=1.35;
-            else if(abs(da[i])<=100) da[i]/=1.58;
-            else da[i]/=1.8;
+            if(abs(da[i])<30) da[i]*=1.1;
+            else if(abs(da[i])<=60) da[i]*=1.35;
+            else if(abs(da[i])<=100) da[i]*=1.58;
+            else da[i]*=1.8;
         }
         if(i==3)
         {
@@ -52,15 +47,33 @@ int* duty_calc(int vel_euc[])
             else if(abs(da[i])<=100) da[i]/=1.6;
             else da[i]/=1.8;
         }
-        // if(i==1 )
-        // {
-        //     if(abs(da[i])<30) da[i]/=1.05;
-        //     else if(abs(da[i])<=60) da[i]/=1.08;
-        //     else if(abs(da[i])<=100) da[i]/=1.09;
-        //     else da[i]/=1.098;
-        // }
+        
+        printf("%d ",da[i]);
+    }*/
+
+    //scaling for vy
+    /*for (int i = 0; i < 4; i++)
+    {
+        da[i]=(int)(127*buf[i]/(25000*3.14/60));
+        printf("%d ",da[i]);
+        if(i==1)
+        {
+            if(abs(da[i])<30) da[i]*=1.1;
+            else if(abs(da[i])<=60) da[i]*=1.2;
+            else if(abs(da[i])<=100) da[i]*=1.3;
+            else da[i]*=1.8;
+        }
+        if(i==3)
+        {
+            if(abs(da[i])<30) da[i]*=1.1;
+            else if(abs(da[i])<=60) da[i]*=1.2;
+            else if(abs(da[i])<=100) da[i]*=1.3;
+            else da[i]*=1.8;
+        }
+        
         printf("%d ",da[i]);
     }
+*/
 
     return da;
 }
@@ -99,6 +112,10 @@ int main(int argc, char const *argv[])
     // buf[4]=0;
    
     buf[5]=0;
+    /*// buf[4]=0;
+    buf[1]=100;
+    buf[2]=0; 
+    buf[3]=0;*/
     printf("sending: %d %d %d %d %d\n", buf[1], buf[2], buf[3], buf[4], buf[5]);
     serial.Write(buf, 6);
   }
